e3c0b5a7bebb705affb12903c1512892583f2753
al マス <al4auuu@gmail.com>
2025-01-22 20:03:00 +0700

    MODULES: Refactor `installer` logic (`101-74437d7-release`)
    - klin ap the `installer` script.
    - prevented `.sha256` files from being extracted into the `system` folder.
    - Added chwck to remove `Bumbu Racik` module if it exists before installation.
    
    Signed-off-by: al4uu
    <al4uu@proton.me> by al マス

--- modules/install.sh
+++ modules/install.sh
@@ -6,10 +6,11 @@ LATESTARTSERVICE=true
 
 ui_print " "
 ui_print "* SnapFest Tweaks"
-ui_print "* Version 1.0 (GIT@d07d596)"
+ui_print "* Version 1.1 (GIT@74437d7)"
 ui_print "* @al4uu & @allprjkt"
 ui_print " "
 sleep 1
+
 ui_print "- Device : $(getprop ro.product.manufacturer), $(getprop ro.product.device)"
 sleep 1
 ui_print "- SELinux Status : $(getenforce)"
@@ -21,11 +22,15 @@ ui_print "- Installing.."
 
 sleep 5
 
-unzip "$ZIPFILE" system/* -d "$MODPATH/" >/dev/null 2>&1
+if [ -d /data/adb/modules/bumbu_racik ]; then
+    ui_print "- Bumbu Racik module detected. Removing.."
+    rm -rf /data/adb/modules/bumbu_racik
+fi
+
+unzip "$ZIPFILE" "system/*" -x "*.sha256" -d "$MODPATH/" >/dev/null 2>&1
 unzip "$ZIPFILE" "action.sh" "snapfest.png" -d "$MODPATH/" >/dev/null 2>&1
 
 cp -af "$TMPDIR"/action.sh "$MODPATH"/action.sh >/dev/null 2>&1
-cp -af "$TMPDIR"/snapfest.png "$MODPATH"/snapfest.png >/dev/null 2>&1
 
 cp -f "$MODPATH"/snapfest.png /data/local/tmp/ >/dev/null 2>&1
 chmod 644 /data/local/tmp/snapfest.png >/dev/null 2>&1
@@ -34,26 +39,17 @@ sleep 5
 
 random=$((RANDOM % 9))
 
-if [ $random -eq 0 ]; then
-  ui_print "- Snap Into Action !"
-elif [ $random -eq 1 ]; then
-  ui_print "- Leave Lag Behind !"
-elif [ $random -eq 2 ]; then
-  ui_print "- Snap the Lag Away !"
-elif [ $random -eq 3 ]; then
-  ui_print "- Optimize. Game. Win !"
-elif [ $random -eq 4 ]; then
-  ui_print "- Stability Meets Power !"
-elif [ $random -eq 5 ]; then
-  ui_print "- Rise with Snapdragon !"
-elif [ $random -eq 6 ]; then
-  ui_print "- Perform Like a Phoenix !"
-elif [ $random -eq 7 ]; then
-  ui_print "- Dominate with Precision !"
-elif [ $random -eq 8 ]; then
-  ui_print "- Frame Stability? LOCKED !"
-else
-  ui_print "- Unleash Snapdragon Efficiency !"
-fi
+case $random in
+  0) ui_print "- Snap Into Action !" ;;
+  1) ui_print "- Leave Lag Behind !" ;;
+  2) ui_print "- Snap the Lag Away !" ;;
+  3) ui_print "- Optimize. Game. Win !" ;;
+  4) ui_print "- Stability Meets Power !" ;;
+  5) ui_print "- Rise with Snapdragon !" ;;
+  6) ui_print "- Perform Like a Phoenix !" ;;
+  7) ui_print "- Dominate with Precision !" ;;
+  8) ui_print "- Frame Stability? LOCKED !" ;;
+  *) ui_print "- Unleash Snapdragon Efficiency !" ;;
+esac
 
 ui_print " "
