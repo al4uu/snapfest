6a049a41955c0d4e7f203961bc5a368bfafa8bdc
al マス <al4auuu@gmail.com>
2025-02-22 14:35:54 +0700

    MODULES: Add more package names and fix `sched_lib` write issues
    Signed-off-by: al4uu <al4uu@proton.me>

--- modules/common/service.sh
+++ modules/common/service.sh
@@ -271,14 +271,15 @@ for service in logd traced statsd mi_thermald; do
     su -c "stop $service"
 done
 
-lib_names="com.miHoYo. com.activision. com.garena. com.roblox. com.epicgames com.dts. UnityMain libunity.so libil2cpp.so libmain.so libcri_vip_unity.so libopus.so libxlua.so libUE4.so libAsphalt9.so libnative-lib.so libRiotGamesApi.so libResources.so libagame.so libapp.so libflutter.so libMSDKCore.so libFIFAMobileNeon.so libUnreal.so libEOSSDK.so libcocos2dcpp.so libgodot_android.so libgdx.so libgdx-box2d.so libminecraftpe.so libLive2DCubismCore.so libyuzu-android.so libryujinx.so libcitra-android.so libhdr_pro_engine.so libandroidx.graphics.path.so libeffect.so"
+lib_names="com.miHoYo. com.activision. com.garena. com.roblox. com.proxima com.tencent com.epicgames com.dts. UnityMain libunity.so libil2cpp.so libmain.so libcri_vip_unity.so libopus.so libxlua.so libUE4.so libAsphalt9.so libnative-lib.so libRiotGamesApi.so libResources.so libagame.so libapp.so libflutter.so libMSDKCore.so libFIFAMobileNeon.so libUnreal.so libEOSSDK.so libcocos2dcpp.so libgodot_android.so libgdx.so libgdx-box2d.so libminecraftpe.so libLive2DCubismCore.so libyuzu-android.so libryujinx.so libcitra-android.so libhdr_pro_engine.so libandroidx.graphics.path.so libeffect.so"
 
 for path in /proc/sys/kernel/sched_lib_name /proc/sys/kernel/sched_lib_mask_force /proc/sys/walt/sched_lib_name /proc/sys/walt/sched_lib_mask_force; do
     if [ -w "$path" ]; then
-        case "$path" in
-            */sched_lib_name) echo "$lib_names" > "$path" ;;
-            */sched_lib_mask_force) echo "255" > "$path" ;;
-        esac
+        if [[ "$path" == */sched_lib_name ]]; then
+            echo "$lib_names" > "$path"
+        elif [[ "$path" == */sched_lib_mask_force ]]; then
+            echo "255" > "$path"
+        fi
     fi
 done
 
