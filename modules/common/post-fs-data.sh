11bb3b9d0be5ea364192ed9b3ec05d354583ecca
al マス <al4auuu@gmail.com>
2025-03-05 00:49:20 +0700

    MODULES: Drop `Dynamic` Description in `post-fs-data`
    Signed-off-by: al4uu <al4uu@proton.me>

--- modules/common/post-fs-data.sh
+++ modules/common/post-fs-data.sh
@@ -1,37 +1,5 @@
 #!/system/bin/sh
 
-ROOT_METHOD="Unknown"
-ROOT_VERSION="Unknown"
-
-if [ -d "/data/adb/ksu" ]; then
-    ROOT_METHOD="KernelSU"
-    if command -v su &>/dev/null; then
-        ROOT_VERSION=$(su --version 2>/dev/null | cut -d ':' -f 1)
-    fi
-elif [ -d "/data/adb/magisk" ]; then
-    ROOT_METHOD="Magisk"
-    if command -v magisk &>/dev/null; then
-        ROOT_VERSION=$(magisk -V)
-    fi
-elif [ -d "/data/adb/ap" ]; then
-    ROOT_METHOD="APatch"
-    if [ -f "/data/adb/ap/version" ]; then
-        ROOT_VERSION=$(cat /data/adb/ap/version)
-    fi
-fi
-
-MODDIR="/data/adb/modules/snapfest"
-MODULE_PROP="${MODDIR}/module.prop"
-BACKUP_PROP="${MODULE_PROP}.orig"
-
-if [ -f "$MODULE_PROP" ] && [ ! -f "$BACKUP_PROP" ]; then
-    cp "$MODULE_PROP" "$BACKUP_PROP"
-fi
-
-if [ -f "$MODULE_PROP" ]; then
-    sed -i "s/^description=.*/description=[ 😋 SnapFest is running | ✅ ${ROOT_METHOD} (${ROOT_VERSION}) ] Special performance module designed for Snapdragon devices !/" "$MODULE_PROP"
-fi
-
 MODPATH=/data/adb/modules/snapfest
 
 mkdir -p "$MODPATH"/system/lib/egl
