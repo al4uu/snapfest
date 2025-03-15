3e98223b7cf08ae909a839987434e9cb65103a4d
al マス <al4auuu@gmail.com>
2025-03-15 18:22:34 +0700

    MODULES: Remove other `props` in `uninstall.sh`
    Signed-off-by: al4uu <al4uu@proton.me>

--- modules/uninstall.sh
+++ modules/uninstall.sh
@@ -9,8 +9,6 @@ find /data/user_de -type f -name '*shaders_cache*' -exec rm -f {} + > /dev/null 
 find /data -type f -name '*shader*' -exec rm -f {} + > /dev/null 2>&1
 
 settings delete global auto_sync
-settings delete global ble_scan_always_enabled
-settings delete global wifi_scan_always_enabled
 settings delete global hotword_detection_enabled
 settings delete global activity_starts_logging_enabled
 settings delete secure adaptive_sleep
@@ -22,5 +20,3 @@ settings delete system air_motion_engine
 settings delete system air_motion_wake_up
 settings delete system send_security_reports
 settings delete system intelligent_sleep_mode
-settings delete system nearby_scanning_enabled
-settings delete system nearby_scanning_permission_allowed
